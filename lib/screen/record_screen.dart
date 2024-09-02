5a4d3adb28f0befd91f3f77aa0b2d9acb30bb245
djcommunity <djcommunity20@gmail.com>
2024-09-02 19:10:44 +0900

    📦️ Record Screen
    add subtle improvement of RecordScreen

--- lib/screen/record_screen.dart
+++ lib/screen/record_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:routin_e/component/excercise_card.dart';
 import '../component/calendar.dart';
 import '../component/routine_record.dart';
+import '../database/dao/record_dao.dart';
+import '../database/drift_database.dart';
 
 class RecordScreen extends StatefulWidget {
   const RecordScreen({super.key});
@@ -14,41 +17,35 @@ class _RecordScreenState extends State<RecordScreen> {
   //선택된 날짜는 이제 여기서는 selectedDate로 관리할 것임
   DateTime selectedDate = DateTime.utc(
       DateTime.now().year, DateTime.now().month, DateTime.now().day);
-
-  final ExerciseCardList = [
-    ExerciseCard(),
-    ExerciseCard(),
-    ExerciseCard(),
-    ExerciseCard(),
-    ExerciseCard()
-  ];
+  List<Routine> records = [];
+  late final RecordDao _recordDao;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    _recordDao = GetIt.I<RecordDao>();
+  }
 
-    return Column(
-        children: [
-          Calendar(
-            selectedDate: selectedDate,
-            onDaySelected: onDaySelected,
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 6),
-                RoutineRecord(),
-                SizedBox(height: 6),
-                RoutineRecord(),
-                SizedBox(height: 6),
-                RoutineRecord(),
-                SizedBox(height: 6),
-                RoutineRecord(),
-                // Add more widgets here
-              ],
-            ),
-          ),
-        ],
-      );
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Calendar(
+        selectedDate: selectedDate,
+        onDaySelected: onDaySelected,
+      ),
+      FutureBuilder(
+          future: _recordDao.getRecordsByDate(selectedDate),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('기록이 없습니다.'));
+            } else {
+              return Center(child: Text('내일채우기'));
+            }
+          }),
+    ]);
   }
 
   void onDaySelected(DateTime touchedDate, DateTime focusedDate) {
