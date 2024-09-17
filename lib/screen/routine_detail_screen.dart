52ff6a199830684997a9621e45b632c69fb51e2f
jessy <yaeunjess@gmail.com>
2024-09-17 17:52:35 +0900

    :bug: [FIX] Error:Could not find the correct Provider<ExerciseSingleState>
    by jessy

--- lib/screen/routine_detail_screen.dart
+++ lib/screen/routine_detail_screen.dart
@@ -77,6 +77,7 @@ class _RoutineDetailScreenState extends State<RoutineDetailScreen> {
         providers: widget.exercises.map((exercise) {
           if (exercise.counterType == CounterType.isDual) {
             return ChangeNotifierProvider(
+              key: ValueKey(exercise.title),
               create: (_) =>
                   ExerciseDualState(counts: [0, 0],
                       memo: '',
@@ -84,6 +85,7 @@ class _RoutineDetailScreenState extends State<RoutineDetailScreen> {
             );
           } else {
             return ChangeNotifierProvider(
+              key: ValueKey(exercise.title),
               create: (_) =>
                   ExerciseSingleState(count: 0,
                       memo: '',
@@ -91,55 +93,58 @@ class _RoutineDetailScreenState extends State<RoutineDetailScreen> {
             );
           }
         }).toList(),
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            surfaceTintColor: Colors.white,
-            backgroundColor: Colors.white,
-            foregroundColor: ORANGE_COLOR,
-            title: Text(
-              widget.routineTitle,
-              style: TextStyle(
-                  color: ORANGE_COLOR,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w600),
+        builder: (context, child) {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              surfaceTintColor: Colors.white,
+              backgroundColor: Colors.white,
+              foregroundColor: ORANGE_COLOR,
+              title: Text(
+                widget.routineTitle,
+                style: TextStyle(
+                    color: ORANGE_COLOR,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600),
+              ),
             ),
-          ),
-          bottomNavigationBar: Padding(
-            padding: const EdgeInsets.only(
-                right: 12.0, left: 12.0, bottom: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      final tempRoutineId = UniqueKey().toString();
-                      List<RecordModel> records = [];
+            bottomNavigationBar: Padding(
+              padding: const EdgeInsets.only(
+                  right: 12.0, left: 12.0, bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final tempRoutineId = UniqueKey().toString();
+                        List<RecordModel> records = [];
 
-                      for (int i = 0; i < widget.exercises.length; i++) {
-                        var exercise = widget.exercises[i];
+                        for (int i = 0; i < widget.exercises.length; i++) {
+                          var exercise = widget.exercises[i];
 
-                        if (exercise.counterType == CounterType.isDual) {
-                          var exerciseState = context.read<ExerciseDualState>();
+                          if (exercise.counterType == CounterType.isDual) {
+                            var exerciseState = Provider.of<ExerciseDualState>(
+                                context, listen: false);
 
-                          records.add(
-                              RecordModel(
-                                id: UniqueKey().toString(),
-                                date: DateTime.now(),
-                                routineTitle: widget.routineTitle,
-                                tempRoutineId: tempRoutineId,
-                                exerciseTitle: exercise.title,
-                                counterType: exercise.counterType,
-                                achievementCounts: exerciseState.counts,
-                              )
-                          );
-                        } else {
-                          var exerciseState = context.read<ExerciseSingleState>();
+                            records.add(
+                                RecordModel(
+                                  id: UniqueKey().toString(),
+                                  date: DateTime.now(),
+                                  routineTitle: widget.routineTitle,
+                                  tempRoutineId: tempRoutineId,
+                                  exerciseTitle: exercise.title,
+                                  counterType: exercise.counterType,
+                                  achievementCounts: exerciseState.counts,
+                                )
+                            );
+                          } else {
+                            var exerciseState = Provider.of<
+                                ExerciseSingleState>(context, listen: false);
 
-                          records.add(
-                              RecordModel(
+                            records.add(
+                                RecordModel(
                                   id: UniqueKey().toString(),
                                   date: DateTime.now(),
                                   routineTitle: widget.routineTitle,
@@ -147,112 +152,114 @@ class _RoutineDetailScreenState extends State<RoutineDetailScreen> {
                                   exerciseTitle: exercise.title,
                                   counterType: exercise.counterType,
                                   achievementCounts: [exerciseState.count],
-                              )
-                          );
+                                )
+                            );
+                          }
                         }
-                      }
 
-                      // DB에 records 저장
-                      for (var record in records) {
-                        final companion = RecordsCompanion(
-                          id: Value(record.id),
-                          date: Value(record.date),
-                          routineTitle: Value(record.routineTitle),
-                          tempRoutineId: Value(record.tempRoutineId),
-                          exerciseTitle: Value(record.exerciseTitle),
-                          counterType: Value(record.counterType),
-                          achievementCounts: Value(record.achievementCounts?.toString() ?? '[]'),
-                        );
+                        // DB에 records 저장
+                        for (var record in records) {
+                          final companion = RecordsCompanion(
+                            id: Value(record.id),
+                            date: Value(record.date),
+                            routineTitle: Value(record.routineTitle),
+                            tempRoutineId: Value(record.tempRoutineId),
+                            exerciseTitle: Value(record.exerciseTitle),
+                            counterType: Value(record.counterType),
+                            achievementCounts: Value(
+                                record.achievementCounts?.toString() ?? '[]'),
+                          );
 
-                        // Record를 DB에 삽입
-                        await _recordDao.insertRecord(companion);
-                      }
+                          // Record를 DB에 삽입
+                          await _recordDao.insertRecord(companion);
+                        }
 
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: GREY_COLOR,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: GREY_COLOR,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
                       ),
-                    ),
-                    child: Text(
-                      '완료하기',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
+                      child: Text(
+                        '완료하기',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 8.0),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: BLUE_COLOR,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: BLUE_COLOR,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
                       ),
-                    ),
-                    child: Text(
-                      '종료하기',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
+                      child: Text(
+                        '종료하기',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          backgroundColor: Colors.white,
-          body: SafeArea(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(widget.exercises.length, (index) {
-                    return Container(
-                      margin: EdgeInsets.symmetric(horizontal: 4.0),
-                      width: _currentPage == index ? 10.0 : 8.0,
-                      height: _currentPage == index ? 10.0 : 8.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: _currentPage == index ? ORANGE_COLOR : Colors
-                            .grey,
-                      ),
-                    );
-                  }),
-                ),
-                SizedBox(height: 16.0),
-                Expanded(
-                  child: PageView.builder(
-                    controller: _pageController,
-                    itemCount: widget.exercises.length,
-                    onPageChanged: (int index) {
-                      setState(() {
-                        _currentPage = index;
-                      });
-                    },
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child:
-                        ExerciseDetailCard(exercise: widget.exercises[index]),
+            backgroundColor: Colors.white,
+            body: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(widget.exercises.length, (index) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: 4.0),
+                        width: _currentPage == index ? 10.0 : 8.0,
+                        height: _currentPage == index ? 10.0 : 8.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentPage == index ? ORANGE_COLOR : Colors
+                              .grey,
+                        ),
                       );
-                    },
+                    }),
                   ),
-                ),
-              ],
+                  SizedBox(height: 16.0),
+                  Expanded(
+                    child: PageView.builder(
+                      controller: _pageController,
+                      itemCount: widget.exercises.length,
+                      onPageChanged: (int index) {
+                        setState(() {
+                          _currentPage = index;
+                        });
+                      },
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child:
+                          ExerciseDetailCard(exercise: widget.exercises[index]),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ),
+          );
+        }
     );
   }
 }
